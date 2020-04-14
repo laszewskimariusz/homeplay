585c8808386f191e3c9c9b40fbe7ba3a153bc086
Mariusz <Laszewskimariusz@gmial.com>
2020-04-14 12:20:16 +0100

    Massive update
    Add login page, template,

--- film/urls.py
+++ film/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 
+from . import views
+from django.urls import path
+
 urlpatterns = [
-    
-    path('', views.index, name='index'),
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]

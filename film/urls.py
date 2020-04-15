d64e82e4eb5761508902f931c1a0a48b490b41fb
Mariusz <Laszewskimariusz@gmial.com>
2020-04-15 09:55:20 +0100

    Aspect ratios
    embed-responsive embed-responsive-16by9

--- film/urls.py
+++ film/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 
-from . import views
-from django.urls import path
-
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
